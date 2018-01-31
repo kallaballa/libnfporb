3f73354c8b4875d43559f559b265b0d963db813c
kallaballa <amir@viel-zu.org>
2018-01-31 13:21:13 +0100

    discard immediate intersection vectors

--- app/nfp.cpp
+++ app/nfp.cpp
@@ -88,7 +88,7 @@ coord_t get_slope(const segment_t& seg, bool invert = false) {
   return atan2(d_x, -d_y);
 }
 
-struct Toucher {
+struct TouchingPoint {
 	enum Type {
 		VERTEX,
 		A_ON_B,
@@ -97,13 +97,6 @@ struct Toucher {
 	Type type_;
 	psize_t A_;
 	psize_t B_;
-};
-
-struct TransVector {
-	coord_t x_;
-	coord_t y_;
-	point_t start_;
-	point_t end_;
 };
 
 template <typename Geometry>
@@ -154,26 +147,29 @@ point_t find_maximum_y(const polygon_t& p) {
 	return result;
 }
 
-std::vector<Toucher> findTouchers(const std::vector<point_t>& ringA, const std::vector<point_t>& ringB) {
-	std::vector<Toucher> touchers;
+std::vector<TouchingPoint> findTouchingPoints(const std::vector<point_t>& ringA, const std::vector<point_t>& ringB) {
+	std::vector<TouchingPoint> touchers;
 	for(psize_t i = 0; i < ringA.size() - 1; i++) {
 		size_t nextI = i+1;
 		for(psize_t j = 0; j < ringB.size() - 1; j++) {
 			size_t nextJ = j+1;
 			if(bg::intersects(ringA[i], ringB[j])) {
-				touchers.push_back({Toucher::VERTEX, i, j});
+				touchers.push_back({TouchingPoint::VERTEX, i, j});
 			} else if (!bg::intersects(ringA[nextI], ringB[j]) && bg::intersects(segment_t(ringA[i],ringA[nextI]), ringB[j])) {
-				touchers.push_back({Toucher::B_ON_A, nextI, j});
+				touchers.push_back({TouchingPoint::B_ON_A, nextI, j});
 			} else if (!bg::intersects(ringB[nextJ], ringA[i]) && bg::intersects(segment_t(ringB[j],ringB[nextJ]), ringA[i])) {
-				touchers.push_back({Toucher::A_ON_B, i, nextJ});
+				touchers.push_back({TouchingPoint::A_ON_B, i, nextJ});
 			}
 		}
 	}
 	return touchers;
 }
 
-std::set<point_t> findPotentialVectors(const std::vector<point_t>& ringA, const std::vector<point_t>& ringB, const std::vector<Toucher>& touchers) {
-	std::set<point_t> transVectors;
+std::set<point_t> findTranslationVectors(const std::vector<point_t>& ringA, const std::vector<point_t>& ringB, const std::vector<TouchingPoint>& touchers) {
+	//use a set to automatically filter duplicate vectors
+	std::set<point_t> potentialVectors;
+	std::vector<std::pair<segment_t,segment_t>> touchEdges;
+
 	for (psize_t i = 0; i < touchers.size(); i++) {
 		point_t vertexA = ringA[touchers[i].A_];
 		vertexA.marked_ = true;
@@ -183,7 +179,7 @@ std::set<point_t> findPotentialVectors(const std::vector<point_t>& ringA, const 
 		signed long nextAindex = touchers[i].A_ + 1;
 
 		prevAindex = (prevAindex < 0) ? ringA.size() - 2 : prevAindex; // loop
-		nextAindex = (nextAindex >= ringA.size()) ? 1 : nextAindex; // loop
+		nextAindex = (static_cast<psize_t>(nextAindex) >= ringA.size()) ? 1 : nextAindex; // loop
 
 		point_t prevA = ringA[prevAindex];
 		point_t nextA = ringA[nextAindex];
@@ -195,71 +191,93 @@ std::set<point_t> findPotentialVectors(const std::vector<point_t>& ringA, const 
 		signed long nextBindex = touchers[i].B_ + 1;
 
 		prevBindex = (prevBindex < 0) ? ringB.size() - 2 : prevBindex; // loop
-		nextBindex = (nextBindex >= ringB.size()) ? 1 : nextBindex; // loop
+		nextBindex = (static_cast<psize_t>(nextBindex) >= ringB.size()) ? 1 : nextBindex; // loop
 
 		point_t prevB = ringB[prevBindex];
 		point_t nextB = ringB[nextBindex];
 
-		if (touchers[i].type_ == Toucher::VERTEX) {
+		if (touchers[i].type_ == TouchingPoint::VERTEX) {
 			segment_t a1 = { vertexA, nextA };
 			segment_t a2 = { prevA, vertexA };
 			segment_t b1 = { vertexB, nextB };
 			segment_t b2 = { prevB, vertexB };
+
+			//swap the segment elements so that always the first point is the touching point
+			touchEdges.push_back({a1, b1});
+			touchEdges.push_back({a1, {b2.second, b2.first}});
+			touchEdges.push_back({{a2.second, a2.first}, b1});
+			touchEdges.push_back({{a2.second, a2.first}, {b2.second, b2.first}});
+
 			std::cerr << a1 << " " << a2 << " " << b1 << " " << b2 << std::endl;
 			write_svg<segment_t>("touchers" + std::to_string(i) + ".svg", {a1,a2,b1,b2});
 
-			//FIXME test parallel edges for floating point stability
+			//TODO test parallel edges for floating point stability
 			Alignment al;
 			//a1 and b1 meet at start vertex
 			al = get_aligment(a1, b1.second);
 			if(al == LEFT) {
-				std::cerr << "left" << std::endl;
-				transVectors.insert(b1.first - b1.second);
+				potentialVectors.insert(b1.first - b1.second);
 			} else if(al == RIGHT) {
-				std::cerr << "right" << (a1.second - a1.first) << std::endl;
-				transVectors.insert(a1.second - a1.first);
+				potentialVectors.insert(a1.second - a1.first);
 			} else {
-				std::cerr << "parallel" << std::endl;
-				transVectors.insert(a1.second - a1.first);
+				potentialVectors.insert(a1.second - a1.first);
 			}
 
 			//a1 and b2 meet at start and end
 			al = get_aligment(a1, b2.first);
 			if(al == LEFT) {
-				std::cerr << "left" << std::endl;
-				//no feasible edge
+				//no feasible translation
 			} else if(al == RIGHT) {
-				std::cerr << "right" << (a1.second - a1.first) << std::endl;
-				transVectors.insert(a1.second - a1.first);
+				potentialVectors.insert(a1.second - a1.first);
 			} else {
-				std::cerr << "parallel" << std::endl;
-				transVectors.insert(a1.second - a1.first);
+				potentialVectors.insert(a1.second - a1.first);
 			}
 
 			//a2 and b1 meet at end and start
 			al = get_aligment(a2, b1.second);
 			if(al == LEFT) {
-				std::cerr << "left" << std::endl;
-				//no feasible edge
+				//no feasible translation
 			} else if(al == RIGHT) {
-				std::cerr << "right" << (b1.first - b1.second) << std::endl;
-				transVectors.insert(b1.first - b1.second);
+				potentialVectors.insert(b1.first - b1.second);
 			} else {
-				std::cerr << "parallel" << std::endl;
-				transVectors.insert(a2.second - a1.first);
+				potentialVectors.insert(a2.second - a1.first);
 			}
-		} else if (touchers[i].type_ == Toucher::B_ON_A) { 		//FIXME: why does svgnest generate two vectors for B_ON_A and A_ON_B?
-			transVectors.insert( { vertexA.x_ - vertexB.x_, vertexA.y_ - vertexB.y_ });
-		} else if (touchers[i].type_ == Toucher::A_ON_B) {
-			transVectors.insert( { vertexA.x_ - vertexB.x_, vertexA.y_ - vertexB.y_ });
+		} else if (touchers[i].type_ == TouchingPoint::B_ON_A) { 		//FIXME: why does svgnest generate two vectors for B_ON_A and A_ON_B?
+			potentialVectors.insert( { vertexA.x_ - vertexB.x_, vertexA.y_ - vertexB.y_ });
+		} else if (touchers[i].type_ == TouchingPoint::A_ON_B) {
+			potentialVectors.insert( { vertexA.x_ - vertexB.x_, vertexA.y_ - vertexB.y_ });
+
 		}
 	}
-	return transVectors;
+
+	//discard immediately intersecting translations
+	std::set<point_t> vectors;
+	for(auto v : potentialVectors) {
+		bool discarded = false;
+		for(auto sp : touchEdges) {
+			point_t& transStart = sp.first.first;
+			point_t transEnd;
+			trans::translate_transformer<coord_t, 2, 2> translate(v.x_, v.y_);
+			boost::geometry::transform(transStart, transEnd, translate);
+			segment_t transSeg(transStart, transEnd);
+			Alignment a1 = get_aligment(transSeg, sp.first.second);
+			Alignment a2 = get_aligment(transSeg, sp.second.second);
+			if(a1 == a2 && a1 != ON) {
+				// both segments are either left or right of the translation segment and the translation is discarded
+				discarded = true;
+				break;
+			}
+		}
+		if(!discarded)
+			vectors.insert(v);
+	}
+	return vectors;
 }
 
 
 int main(int argc, char** argv) {
 	//TODO: tweak bg's floating point tolerance
+	//TODO: remove superfluous points
 	polygon_t pA;
 	polygon_t pB;
 	polygon_t pifsB;
@@ -277,14 +295,10 @@ int main(int argc, char** argv) {
 	trans::translate_transformer<coord_t, 2, 2> translate(transB.x_, transB.y_);
 	boost::geometry::transform(pB, pifsB, translate);
 	write_svg<polygon_t>("ifs.svg", {pA, pifsB});
-	pB = pifsB;
+	pB = std::move(pifsB);
 
-
-	auto& pAO = pA.outer();
-	auto& pBO = pB.outer();
-
-	std::vector<Toucher> touchers = findTouchers(pA.outer(), pB.outer());
-	std::set<point_t> transVectors = findPotentialVectors(pA.outer(), pB.outer(), touchers);
+	std::vector<TouchingPoint> touchers = findTouchingPoints(pA.outer(), pB.outer());
+	std::set<point_t> transVectors = findTranslationVectors(pA.outer(), pB.outer(), touchers);
 
 	std::cerr << "collected vectors: " << transVectors.size() << std::endl;
 	for(auto pt : transVectors) {
