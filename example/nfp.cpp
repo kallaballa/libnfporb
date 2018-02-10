9886c47abf30f1ed4dccdc3bf2721767a7bff516
kallaballa <amir@viel-zu.org>
2018-02-10 15:27:50 +0100

    librarified

---
+++ example/nfp.cpp
@@ -0,0 +1,47 @@
+#include "../src/libnfp.hpp"
+
+int main(int argc, char** argv) {
+	polygon_t pA;
+	polygon_t pB;
+
+	read_polygon(argv[1], pA);
+	read_polygon(argv[2], pB);
+
+	/*
+  std::random_device rd;
+  std::mt19937 mt(rd());
+  std::uniform_real_distribution<double> dist(-1.0, 1.0);
+
+	for(auto& ptA : pA.outer()) {
+		ptA.x_ += dist(mt);
+		ptA.y_ += dist(mt);
+	}
+	pA.outer().back() = pA.outer().front();
+
+	for(auto& ptB : pB.outer()) {
+		ptB.x_ += dist(mt);
+		ptB.y_ += dist(mt);
+	}
+	pB.outer().back() = pB.outer().front();
+
+	for(auto& rA : pA.inners()) {
+		for(auto& ptA : rA) {
+			ptA.x_ += dist(mt);
+			ptA.y_ += dist(mt);
+		}
+		rA.back() = rA.front();
+	}
+
+	for(auto& rB : pB.inners()) {
+		for(auto& ptB : rB) {
+			ptB.x_ += dist(mt);
+			ptB.y_ += dist(mt);
+		}
+		rB.back() = rB.front();
+	}
+*/
+	nfp_t nfp = generateNFP(pA, pB);
+
+	return 0;
+}
+
