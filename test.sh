5645f2dad737ee0801cca8c4b17d5142c73c98fc
kallaballa <amir@viel-zu.org>
2018-02-14 12:16:40 +0100

    refined test scripts

--- test.sh
+++ test.sh
@@ -1,6 +1,8 @@
+truncate --size 0 test.log
+
 ls -d data/* | while read line; do
   echo -n "#### Running case $line: "
-  examples/nfp $line/A.wkt $line/B.wkt &> test.log
+  examples/nfp $line/A.wkt $line/B.wkt &>> test.log
   if [ $? -eq 0 ]; then
     echo Success
   else
--- testfuzz.sh
+++ testfuzz.sh
@@ -1,6 +1,8 @@
+truncate --size 0 testfuzz.log
+
 ls -d data/* | while read line; do
   echo -n "#### Running case $line: "
-  examples/fuzz $line/A.wkt $line/B.wkt &> testfuzz.log
+  examples/fuzz $line/A.wkt $line/B.wkt &>> testfuzz.log
   if [ $? -eq 0 ]; then
     echo Success
   else
