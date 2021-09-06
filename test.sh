83e32049a4907397c2d0697f99ce2d98bfccf6b1
kallaballa <amir@viel-zu.org>
2021-09-06 17:35:59 +0200

    fixed pitfall in test.sh

--- test.sh
+++ test.sh
@@ -1,12 +1,18 @@
 truncate --size 0 test.log
 
+if [ ! -x examples/nfp ]; then
+  echo "You need to build the examples to run the tests"
+  exit 2
+fi
+
 ls -d data/handcrafted/* | while read line; do
   echo -n "#### Running case $line: "
   examples/nfp $line/A.wkt $line/B.wkt &>> test.log
   if [ $? -eq 0 ]; then
     echo Success
+#    mv nfp.svg `basename $line`-nfp.svg  
   else
     echo Fail
-    exit 1;
+    exit 1
   fi
 done
